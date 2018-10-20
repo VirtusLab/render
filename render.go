fe65a21c35d90494b50eb58f190232215c5c8092
antoniaklja <antoniaklja@gmail.com>
2018-10-20 14:44:27 +0200

    Extra params handling and minor refactoring

--- render.go
+++ render.go
@@ -7,19 +7,62 @@ import (
 	"io/ioutil"
 	"bytes"
 	"github.com/ghodss/yaml"
+	"strings"
 )
 
-func Render(input, output, config string, extraParams []string) error {
-	if !NotEmptyAndExists(input) {
-		log.Fatalf("File %v is empty or does not exist", input)
-	}
-	if !NotEmptyAndExists(config) {
-		log.Fatalf("File %v is empty or does not exist", config)
-	}
+// TODO improve error handling fatals -> error
 
+func Render(input, output, config string, extraParams []string) error {
+	configuration := ParseConfiguration(config, extraParams)
+	parsedTemplate := ParseTemplate(input)
+	render(parsedTemplate, configuration, output)
+
+	return nil
+}
+
+func render(tmpl *template.Template, configuration map[string]interface{}, output string) {
 	stdout := false
 	if len(output) == 0 {
 		stdout = true
+	}
+
+	var buffer bytes.Buffer
+	err := tmpl.Execute(&buffer, configuration)
+	if err != nil {
+		log.Fatal("Can't render template file", err)
+	}
+
+	if stdout {
+		log.Print(buffer.String())
+	} else {
+		err := ioutil.WriteFile(output, buffer.Bytes(), 0644)
+		if err != nil {
+			log.Fatal("Can't save rendered file", err)
+		}
+	}
+}
+
+func ParseTemplate(input string) *template.Template {
+	if !NotEmptyAndExists(input) {
+		log.Fatalf("Template file %v is empty or does not exist", input)
+	}
+
+	b, err := ioutil.ReadFile(input)
+	if err != nil {
+		log.Fatal("Can't open template file", err)
+	}
+
+	parsed, err := template.New(input).Parse(string(b))
+	if err != nil {
+		log.Fatal("Can't parse template file", err)
+	}
+
+	return parsed
+}
+
+func ParseConfiguration(config string, extraParams []string) map[string]interface{} {
+	if !NotEmptyAndExists(config) {
+		log.Fatalf("Config file %v is empty or does not exist", config)
 	}
 
 	var configMap map[string]interface{}
@@ -32,35 +75,14 @@ func Render(input, output, config string, extraParams []string) error {
 		log.Fatal("Can't parse config file", err)
 	}
 
-	b, err = ioutil.ReadFile(input)
-	if err != nil {
-		log.Fatal("Can't open template file", err)
-	}
-
-	parsed, err := template.New(input).Parse(string(b))
-	if err != nil {
-		log.Fatal("Can't parse template file", err)
-	}
-
-	var buffer bytes.Buffer
-	err = parsed.Execute(&buffer, configMap)
-	if err != nil {
-		log.Fatal("Can't render template file", err)
-	}
-
-	rendered := buffer.String()
-	if stdout {
-		log.Printf("Rendering %v", input)
-		log.Print(rendered)
-	} else {
-		err := ioutil.WriteFile(output, buffer.Bytes(), 0644)
-		log.Printf("Rendering %v and saving %v", input, output)
-		if err != nil {
-			log.Fatal("Can't save rendered file", err)
+	for _, v:= range extraParams {
+		if strings.Contains(v, "=") {
+			pair := strings.Split(v, "=")
+			configMap[pair[0]] = pair[1]
 		}
 	}
 
-	return nil
+	return configMap
 }
 
 func NotEmptyAndExists(file string) bool {
